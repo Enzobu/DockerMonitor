59a162a820063f42764f1c97a9a945da20a07d45
Enzo Palermo <palermo.enzo.ep@gmail.com>
2025-01-17 23:16:45 +0100

    add icons and config paths to app

--- control_panel.py
+++ control_panel.py
@@ -6,3 +6,15 @@ from PyQt5.QtCore import QTimer
 import os
 import json
 
+# Chemins des icônes
+icon_paths = {
+    0: "/opt/dockerExtension/dockerIcon.png",       # Rouge : application arrêtée
+    1: "/opt/dockerExtension/dockerIconStandBy.png",# Orange : standby (uniquement en quittant)
+    2: "/opt/dockerExtension/dockerIconRun.png"     # Vert : application démarrée
+}
+project_icon_paths = {
+    0: "/opt/dockerExtension/icon_inactive.png",    # Rouge : projet arrêté
+    1: "/opt/dockerExtension/icon_standBy.png",     # Orange : standby
+    2: "/opt/dockerExtension/icon_active.png"       # Vert : projet démarré
+}
+config_path = "/home/enzo/Dev/config.json"
