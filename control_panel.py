fa0980e6e9fdaa84486c8ddcb45f4970b5509aa9
Enzo Palermo <palermo.enzo.ep@gmail.com>
2025-01-17 23:16:14 +0100

    create py app

---
+++ control_panel.py
@@ -0,0 +1,8 @@
+import sys
+import subprocess
+from PyQt5.QtWidgets import QApplication, QSystemTrayIcon, QMenu, QAction
+from PyQt5.QtGui import QIcon
+from PyQt5.QtCore import QTimer
+import os
+import json
+
